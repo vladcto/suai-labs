0eb6afe2056bd756d674c3ad6ce287ae71f4119a
vladcto <55505918+vladcto@users.noreply.github.com>
2024-02-13 00:17:03 +0300

    Update constraints

--- 6_semester/МСПИСТ/1/init.sql
+++ 6_semester/МСПИСТ/1/init.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS university
 CREATE TABLE IF NOT EXISTS faculty
 (
     id            INT PRIMARY KEY AUTO_INCREMENT,
-    university_id INT,
+    university_id INT NOT NULL,
     number        INT NOT NULL UNIQUE,
     FOREIGN KEY (university_id) REFERENCES university (id)
 );
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS faculty
 CREATE TABLE IF NOT EXISTS uni_group
 (
     id         INT PRIMARY KEY AUTO_INCREMENT,
-    faculty_id INT,
+    faculty_id INT         NOT NULL,
     name       VARCHAR(50) NOT NULL UNIQUE,
     FOREIGN KEY (faculty_id) REFERENCES faculty (id)
 );
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS uni_group
 CREATE TABLE IF NOT EXISTS student
 (
     id       INT PRIMARY KEY AUTO_INCREMENT,
-    group_id INT,
+    group_id INT          NOT NULL,
     name     VARCHAR(100) NOT NULL,
     CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES uni_group (id)
 );
@@ -41,14 +41,14 @@ CREATE TABLE IF NOT EXISTS conference
 
 CREATE TABLE IF NOT EXISTS topic
 (
-    id            INT PRIMARY KEY AUTO_INCREMENT,
-    title         VARCHAR(255) NOT NULL
+    id    INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS authorship
 (
-    author_id INT,
-    topic_id  INT,
+    author_id INT NOT NULL,
+    topic_id  INT NOT NULL,
     CONSTRAINT pk_authorship PRIMARY KEY (author_id, topic_id),
     CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES student (id),
     CONSTRAINT fk_topic_id FOREIGN KEY (topic_id) REFERENCES topic (id)
@@ -57,8 +57,8 @@ CREATE TABLE IF NOT EXISTS authorship
 CREATE TABLE IF NOT EXISTS conference_program
 (
     program_id    INT PRIMARY KEY AUTO_INCREMENT,
-    conference_id INT,
-    topic_id      INT,
+    conference_id INT  NOT NULL,
+    topic_id      INT  NOT NULL,
     start_time    TIME NOT NULL,
     end_time      TIME NOT NULL,
     CONSTRAINT fk_conf_id_program FOREIGN KEY (conference_id) REFERENCES conference (id),
