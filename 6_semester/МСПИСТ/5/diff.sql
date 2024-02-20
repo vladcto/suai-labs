1739790205480258cf6312a40a422df84e07ac6a
vladcto <55505918+vladcto@users.noreply.github.com>
2024-02-20 04:07:10 +0300

    Добавил create_view

--- 6_semester/МСПИСТ/5/diff.sql
+++ 6_semester/МСПИСТ/5/diff.sql
@@ -1,27 +1,26 @@
 USE conference_db_lab1;
 
--- Все студенты, которые являются авторами хотя бы одной темы
-SELECT s.name
-    FROM student s
-    WHERE EXISTS (SELECT 1
-                      FROM authorship a
-                      WHERE a.author_id = s.id);
-
--- Всех студенты, которые не являются авторами ни одной темы
-SELECT s.name
-    FROM student s
-    WHERE NOT EXISTS (SELECT 1
-                          FROM authorship a
-                          WHERE a.author_id = s.id);
+DROP VIEW IF EXISTS authors;
+CREATE VIEW authors AS
+SELECT a.author_id
+    FROM authorship a;
 
 -- Все студенты, которые являются авторами хотя бы одной темы
 SELECT s.name
     FROM student s
-    WHERE s.id IN (SELECT a.author_id
-                       FROM authorship a);
+    WHERE s.id IN (SELECT author_id FROM authors);
 
 -- Всех студенты, которые не являются авторами ни одной темы
 SELECT s.name
     FROM student s
-    WHERE s.id NOT IN (SELECT a.author_id
-                           FROM authorship a);
+    WHERE s.id NOT IN (SELECT author_id FROM authors);
+
+-- Все студенты, которые являются авторами хотя бы одной темы
+SELECT s.name
+    FROM student s
+    WHERE s.id IN (SELECT author_id FROM authors);
+
+-- Всех студенты, которые не являются авторами ни одной темы
+SELECT s.name
+    FROM student s
+    WHERE s.id NOT IN (SELECT author_id FROM authors);
