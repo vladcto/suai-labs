3319b03f1bf31badd50e5afb4d7a5331305b85f2
vladcto <55505918+vladcto@users.noreply.github.com>
2024-02-25 03:14:25 +0300

    fix sql for 6 lab

--- 6_semester/МСПИСТ/6/calculate_student.sql
+++ 6_semester/МСПИСТ/6/calculate_student.sql
@@ -1,18 +1,20 @@
+-- Вычисление и возврат значения агрегатной функции
 USE conference_db_lab1;
 
 DELIMITER //
 CREATE FUNCTION GetReportCountForUniversity(university_id INT) RETURNS INT
 BEGIN
-    DECLARE report_count INT;
+  DECLARE report_count INT;
 
-    SELECT COUNT(*) INTO report_count
+  SELECT COUNT(*)
+    INTO report_count
     FROM authorship
-    JOIN student ON authorship.author_id = student.id
-    JOIN uni_group ON student.group_id = uni_group.id
-    JOIN faculty ON uni_group.faculty_id = faculty.id
+           JOIN student ON authorship.author_id = student.id
+           JOIN uni_group ON student.group_id = uni_group.id
+           JOIN faculty ON uni_group.faculty_id = faculty.id
     WHERE faculty.university_id = university_id;
 
-    RETURN report_count;
+  RETURN report_count;
 END//
 DELIMITER ;
 
