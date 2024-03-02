9bf40af62bf5e212a34882af1abb913f41e97654
vladcto <55505918+vladcto@users.noreply.github.com>
2024-03-02 20:08:56 +0300

    Доделал отчет по 3 и 4 лабе

--- 6_semester/Web/4/scripts/submit.js
+++ 6_semester/Web/4/scripts/submit.js
@@ -1,9 +1,4 @@
-// Обработчик события отправки формы
 document.getElementById('booking-form').addEventListener('submit', function (event) {
-	// Предотвращаем отправку формы по умолчанию
-	event.preventDefault();
-
-	// Считываем данные из формы
 	const name = document.querySelector('input[name="name"]').value;
 	const comments = document.querySelector('textarea[name="comments"]').value;
 	const password = document.querySelector('input[name="password"]').value;
@@ -12,12 +7,13 @@ document.getElementById('booking-form').addEventListener('submit', function (eve
 	const lunch = document.querySelector('input[name="lunch"]').checked;
 	const date = document.querySelector('input[name="date"]').value;
 
-	// Выводим данные в консоль
 	console.log('Name:', name);
 	console.log('Comments:', comments);
 	console.log('Password:', password);
 	console.log('Room type:', roomType);
-	console.log('Breakfast included:', breakfast);
-	console.log('Lunch included:', lunch);
+	console.log('Breakfast:', breakfast);
+	console.log('Lunch:', lunch);
 	console.log('Date:', date);
+
+	event.preventDefault();
 });
